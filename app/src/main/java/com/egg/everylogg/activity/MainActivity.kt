433686c11232f1420ca7f6bbf826ef80795f5f4b
coldnoodle_kjy <niwa32329@naver.com>
2022-11-06 20:58:01 +0900

    xml완성파일push
    push xml files in android project

---
+++ app/src/main/java/com/egg/everylogg/activity/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.egg.everylogg.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.egg.everylogg.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //메인 액티비티
+    }
+}
