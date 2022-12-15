8a84a95292196d1ffd9ff46dbb622375e1bab92c
seohee0925 <appleid21@sookmyung.ac.kr>
2022-12-16 05:13:55 +0900

    First Commit

---
+++ everylog_seohee/app/build/generated/source/buildConfig/debug/com/seohee/everylog_seohee/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.seohee.everylog_seohee;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.seohee.everylog_seohee";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
